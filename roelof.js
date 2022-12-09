3aadf38d78472159aa979bc2113c0620dadb3859
RoelofWobben <r.wobben@home.nl>
2022-12-09 10:06:32 +0100

    done

--- roelof.js
+++ roelof.js
@@ -1,8 +1,10 @@
 // zoek de arrow 
 
-visible_button = document.querySelector(".arrow");
+visible_button = document.querySelector(".share");
 popup = document.querySelector(".active");
-hidden_button = document.querySelector(".arrow2");  
+hidden_button = document.querySelector(".share2");
+arrow = document.querySelector('.arrow'); 
+  
 
 // add a eventlistener to the visible button 
 
@@ -16,4 +18,17 @@ hidden_button.addEventListener('click', () => {
     popup.style.display = "none";
 })
 
+
+// add a evenlistener to the share so hover works well
+
+visible_button.addEventListener('mouseover', () => {
+    visible_button.style.backgroundColor = "hsl(217, 19%, 35%)";
+    visible_button.style.cursor = "pointer"
+    arrow.style.fill= "white"
+});
+
+visible_button.addEventListener('mouseleave', () => {
+    visible_button.style.backgroundColor = "lightgray"
+    arrow.style.fill= "hsl(217, 19%, 35%)";
+});
 
